8229405511ad89d2ff260b8757abb8894d50e795
SimoneErba <simone.erba.95@gmail.com>
2026-07-14 17:45:16 +0200

    posthog

---
+++ src/analytics.ts
@@ -0,0 +1,22 @@
+import posthog from 'posthog-js'
+
+const POSTHOG_PROJECT_API_KEY = 'phc_Cke8VNUGFWiYGJePxTwI4GcwUZqDN03mDI2mqDgZjtC'
+const POSTHOG_API_HOST = 'https://us.i.posthog.com'
+
+let initialized = false
+
+export function initAnalytics() {
+  if (initialized || typeof window === 'undefined') {
+    return
+  }
+
+  initialized = true
+
+  posthog.init(POSTHOG_PROJECT_API_KEY, {
+    api_host: POSTHOG_API_HOST,
+    capture_pageview: true,
+    person_profiles: 'identified_only',
+  })
+}
+
+export { posthog }
